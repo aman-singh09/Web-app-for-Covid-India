64ef3488c8e5022c13f9f19444180ffb65e59602
Aman Kumar Singh <amankumar09jul@gmail.com>
2021-03-06 22:09:00 +0530

    Final Code

--- bar.py
+++
@@ -1,8 +0,0 @@
-import requests
-import matplotlib.pyplot as plt
-import pandas as pd
-
-result = requests.get(
-    "https://api.apify.com/v2/key-value-stores/toDWvRj1JpTXiM8FF/records/LATEST?disableRedirect=true")
-content = result.json()
-df = pd.DataFrame(content["regionData"])
